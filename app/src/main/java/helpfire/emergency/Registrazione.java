9be3328129bff99e1ba5a6062d4c92153f13cd5a
ftassatone <f.tassatone@studenti.unisa.it>
2018-06-01 11:13:18 +0200

    Initial commit

---
+++ app/src/main/java/helpfire/emergency/Registrazione.java
@@ -0,0 +1,13 @@
+package helpfire.emergency;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registrazione extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrazione);
+    }
+}
